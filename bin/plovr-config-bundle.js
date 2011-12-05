4c96911ddd6aa4440134f79397d3c7d4cb598581
Daniel Pupius <dan@8th-floor.net>
2011-12-05 13:39:37 -0800

    Add output wrapper to bundle.

--- bin/plovr-config-bundle.js
+++ bin/plovr-config-bundle.js
@@ -7,6 +7,7 @@
   "externs": [
     "./surf-externs.js"
   ],
+  "output-wrapper": "// Copyright 2011 Daniel Pupius <https://github.com/dpup/surface>\n(function(){%output%})();",
   "mode": "ADVANCED",
   "level": "VERBOSE"
 }
