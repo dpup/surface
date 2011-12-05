3298bd9edcad6983b0cbde3f449a4873f06085ae
Daniel Pupius <dan@8th-floor.net>
2011-12-05 13:48:12 -0800

    Add Apache Licence boilerplate

--- interfaces.js
+++ interfaces.js
@@ -1,3 +1,18 @@
+// Copyright 2011 Daniel Pupius  https://github.com/dpup/surface
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 /**
  * @fileoverview Interfaces for implementing individual screens.  A screen factory is used by the
  * app to construct Screen instances for a given navigation.  The screen is then responsible for
